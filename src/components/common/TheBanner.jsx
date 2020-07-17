54a963bac2463fbb7e73d70b3ffb380a48781f8e
Yasunari t <u7df25c588m737@gmail.com>
2020-07-17 19:14:10 +0900

    🔨fix: created truncate banner discription

--- src/components/common/TheBanner.jsx
+++ src/components/common/TheBanner.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react"
 import axios, { requests } from "plugins/axios"
-
+// CSS
+import "./TheBanner.css"
 function TheBanner() {
   const [movie, setMovie] = useState([])
 
@@ -17,6 +18,10 @@ function TheBanner() {
     fetchData()
   }, [])
 
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str
+  }
+
   return (
     <header
       className="banner"
@@ -27,12 +32,16 @@ function TheBanner() {
       }}
     >
       <div className="banner__contents">
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
-        <h1 className="banner__description">{movie?.overview}</h1>
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div>
     </header>
   )
